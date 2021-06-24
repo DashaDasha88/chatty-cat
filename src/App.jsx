44113ac9f7859e4186c2d8f5aa54f7d329d57409
Dasha <dasha.kotova.mail@gmail.com>
2021-06-24 19:02:36 -0400

    add chatfeed component and other basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+
+import "./App.css";
 
 const App = () => {
+
   return (
-    <div>
-      
-    </div>
-  )
+      <ChatEngine
+        height="100vh"
+        projectID="4e8efe3f-24e2-415f-a8e5-0a4b9eb74ff1"
+        userName="ChatCat"
+        userSecret="123123"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+
+      />
+  );
 }
 
 export default App;
