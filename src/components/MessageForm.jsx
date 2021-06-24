7765b9acb291d975d6649632727a9bef27df86ec
Dasha <dasha.kotova.mail@gmail.com>
2021-06-24 19:33:04 -0400

    add message form component

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MessageForm = () => {
+
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form className="message-form" onSubmit={handleSubmit}>
+
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+
+    </form>
   )
 }
 
